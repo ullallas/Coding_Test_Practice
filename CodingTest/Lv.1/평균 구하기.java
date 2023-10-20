858a0f55a1c87414ec91bc8537aee4feb96d3df7
ullallas <chou1520@yonsei.ac.kr>
2023-10-20 11:45:01 +0900

    레벨 1 업로드 완료

---
+++ CodingTest/Lv.1/평균 구하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for (int i=0; i<arr.length; i++) {
+            answer = (answer + arr[i]);
+        }
+        return answer/arr.length;
+    }
+}
+
+/*
+* 문제 설명
+  정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+*/
