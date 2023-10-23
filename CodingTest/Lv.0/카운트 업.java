08e981cd1854d2c05ca2064e54c7f1787c0b493d
ullallas <chou1520@yonsei.ac.kr>
2023-10-23 10:54:07 +0900

    카운트 업

---
+++ CodingTest/Lv.0/카운트 업.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        
+        for (int i=0; i<=end_num-start_num; i++) {
+            answer[i] = start_num + i;
+        }
+        
+        return answer;
+    }
+}
+
+/*
+* 문제 설명
+  정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+*/
