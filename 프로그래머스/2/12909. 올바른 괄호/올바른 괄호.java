7a898b613578f46d84ab0853f759e69ce614abe7
HEE <104607224+ullallas@users.noreply.github.com>
2024-04-01 17:51:53 +0900

    [level 2] Title: 올바른 괄호, Time: 18.15 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
